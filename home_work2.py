f4c5ce9a6fd106f7d8e7301e4bec0020970a591a
КС <xr1st1a2@gmail.com>
2017-11-18 15:39:13 +0200

    Доделал домашнее задание.

--- home_work2.py
+++ home_work2.py
@@ -19,4 +19,26 @@ surname = lst[1]
 student2 = surname + " " + name
 print(student2)
 
-print("======================")
+print("==================================")
+
+snake_style = 'Defence_Of_The_Ancients'
+lst = snake_style.split('_')
+camelized_style = '%s%s%s%s' % (lst[0], lst[1], lst[2], lst[3])
+print(camelized_style)
+
+print("==================================")
+
+s = 'Sergey Yesenin*1895-10-03*1925-12-28'
+lst = s.split('*')
+print(lst)
+writer = s.split("*")[0]
+life = lst[1]
+death = lst[2]
+year1 = life.split('-')[0]
+year2 = death.split('-')[0]
+age = int(year2) - int(year1)
+result ="%s, %d," % (writer, age)
+print(result)
+
+
+
