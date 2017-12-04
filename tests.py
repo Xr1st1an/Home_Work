feaca5d30ee6595764d00acef330c7e5ddb7c074
КС <xr1st1a2@gmail.com>
2017-12-04 21:44:51 +0200

    1-3

---
+++ tests.py
@@ -0,0 +1,23 @@
+#Example 1
+a = 2
+b = 4
+c = 6
+equation = (a + b * c)**2
+result = ('При a = %d, b = %d, c = %d, равно %d ') % (a, b, c, equation)
+print(result)
+
+#Example 2
+a = 20
+b = 30
+c = 40
+equation = a - 4 * b / c
+result = ('При a = %d, b = %d, c = %d, равно %d') % (a, b, c, equation)
+print(result)
+
+#Example 3
+a = 50
+b = 100
+c = 150
+equation = (a * b + 4) / (c - 1)
+result = ('При a = %d, b = %d, c = %d, равно %d') % (a, b, c, equation)
+print(result)
