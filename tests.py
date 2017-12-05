5313eca833114cea17b89b6243e95296dccebc49
КС <xr1st1a2@gmail.com>
2017-12-05 12:50:14 +0200

    1-3

--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
+import math
+
 #Example 1
 a = 2
 b = 4
 c = 6
 equation = (a + b * c)**2
-result = ('При a = %d, b = %d, c = %d, равно %d ') % (a, b, c, equation)
+result = ('При a = %d, b = %d, c = %d, равно %.4f ') % (a, b, c, equation)
 print(result)
 
 #Example 2
@@ -11,7 +13,7 @@ a = 20
 b = 30
 c = 40
 equation = a - 4 * b / c
-result = ('При a = %d, b = %d, c = %d, равно %d') % (a, b, c, equation)
+result = ('При a = %d, b = %d, c = %d, равно %.4f') % (a, b, c, equation)
 print(result)
 
 #Example 3
@@ -19,5 +21,5 @@ a = 50
 b = 100
 c = 150
 equation = (a * b + 4) / (c - 1)
-result = ('При a = %d, b = %d, c = %d, равно %d') % (a, b, c, equation)
+result = ('При a = %d, b = %d, c = %d, равно %.4f') % (a, b, c, equation)
 print(result)
